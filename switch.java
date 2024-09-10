35b9c2b73945f948a5a8fece07bda413f095446a
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-09-10 15:38:58 -0700

    Update switch.java

--- switch.java
+++ switch.java
@@ -1,14 +1,27 @@
+#include "Button.h"
+
+int redLEDPin = 13; 
+int whiteLEDPin = 12;
+Button btn = Button(2);
+int delayTime = 500;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(whiteLEDPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(2) == HIGH) {
-    digitalWrite(13, LOW);
-  } else {
-    digitalWrite(13, HIGH);
+  if (btn.pressed()) {
+    digitalWrite(redLEDPin, HIGH);
+    digitalWrite(whiteLEDPin, LOW);
+    delay(delayTime);
+    digitalWrite(redLEDPin, LOW);
+    digitalWrite(whiteLEDPin, HIGH);
+    delay(delayTime);
+  } else if (btn.released()) {
+    digitalWrite(redLEDPin, LOW);
+    digitalWrite(whiteLEDPin, LOW);
   }
 }

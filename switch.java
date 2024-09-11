6b473201b663fbff91982387ebbfa88a56dcc099
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-09-11 14:34:26 -0700

    Update switch.java

--- switch.java
+++ switch.java
@@ -5,23 +5,38 @@ int whiteLEDPin = 12;
 Button btn = Button(2);
 int delayTime = 500;
 
+bool isFlickering = false; //tracks if LEDs should flicker 
+bool toggleState = false; //tracks which LED is on 
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(redLEDPin, OUTPUT);
   pinMode(whiteLEDPin, OUTPUT);
+  digitalWrite(redLEDPin, LOW);
+  digitalWrite(whiteLEDPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (btn.pressed()) {
-    digitalWrite(redLEDPin, HIGH);
-    digitalWrite(whiteLEDPin, LOW);
-    delay(delayTime);
-    digitalWrite(redLEDPin, LOW);
-    digitalWrite(whiteLEDPin, HIGH);
-    delay(delayTime);
-  } else if (btn.released()) {
+    isFlickering = true; 
+  }
+
+  if (btn.released()) {
+    isFlickering = false; 
     digitalWrite(redLEDPin, LOW);
     digitalWrite(whiteLEDPin, LOW);
+  }
+
+  if (isFlickering) {
+    if (toggleState) {
+      digitalWrite(redLEDPin, HIGH);
+      digitalWrite(whiteLEDPin, LOW);
+    } else {
+      digitalWrite(redLEDPin, LOW);
+      digitalWrite(whiteLEDPin, HIGH);
+    }
+    toggleState = !toggleState;
+    delay(delayTime);
   }
 }
